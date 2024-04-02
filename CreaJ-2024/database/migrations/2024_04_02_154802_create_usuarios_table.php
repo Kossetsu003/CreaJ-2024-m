4c850a94ba6b1e529468069c90c9ffe3db5b79c1
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-04-02 11:51:58 -0600

    first bd

---
+++ CreaJ-2024/database/migrations/2024_04_02_154802_create_usuarios_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario')->unique();
+            $table->string('contrasena');
+            $table->string('nombre')->nullable();
+            $table->string('apellido');
+            $table->double('telefono');
+            $table->string('sexo');
+            $table-> string('rol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
