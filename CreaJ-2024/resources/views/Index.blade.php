089d43f0d1a3e86573e3ae8ffcf4b095012094d4
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-04-09 11:02:52 -0600

    Implementacion de tailwind
    Implementacion de tailwind

---
+++ CreaJ-2024/resources/views/Index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      @vite('resources/css/app.css')
+    <title>Index</title>
+</head>
+<body>
+    <h1 class="text-red-600">Hola</h1>
+</body>
+</html>
