509162aa7d0feb330781d2d4479c17541b8f2d0a
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-04-09 09:56:03 -0600

    Correccion
    Correccion de linea 32

--- CreaJ-2024/database/migrations/2024_04_02_174040_create_producto_table.php
+++ CreaJ-2024/database/migrations/2024_04_02_174040_create_producto_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('producto');
+        Schema::dropIfExists('producto_exhibicion');
     }
 };
