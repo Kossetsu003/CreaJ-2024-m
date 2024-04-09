1d3b9be49ea266c7d93bcf75eb63ea5dcc4b5fdb
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-04-09 11:45:08 -0600

    Creacion tabla venta
    Creacion tabla venta

--- CreaJ-2024/database/migrations/2024_04_09_170457_create_users_table.php
+++ CreaJ-2024/database/migrations/2024_04_09_170457_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('usuario')->unique();
+            $table->string('usuario');
             $table->string('contrasena');
             $table->string('nombre')->nullable();
             $table->string('apellido');
