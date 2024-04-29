ef7b711c10743555f11faac9211af0a552443045
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-04-29 08:11:09 -0600

    Ligero Cambio de diseño
    Ligero Cambio de diseño

--- CreaJ-2024/resources/views/Index.blade.php
+++ CreaJ-2024/resources/views/Index.blade.php
@@ -14,12 +14,12 @@
     <div class="lg:ml-10"> <!-- Sección de texto -->
         <div class="font-bold text-center mb-4">Explore the App</div>
         <div class="w-full md:w-4/5 mx-auto"> <!-- Contenedor de texto -->
-            <p class="text-center mb-4">
+            <p class="text-center mb-4 w-[90%] mx-auto">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex error quia reiciendis est excepturi quasi?
                 Aperiam dolor neque fugit in repudiandae, ipsa distinctio error eius, libero autem ipsum id iste.
             </p>
             <div class="flex justify-center">
-                <button class="bg-blue-300 w-full md:w-[90%] rounded-md font-bold h-9">Let's Start</button>
+                <button class="bg-purple-400 w-[85%] rounded-md font-bold h-9">Let's Start</button>
             </div>
         </div>
     </div>
