88b4ce5e7f8cd17ba46b0503fbad2f3b7d3275c1
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-05-06 09:32:23 -0600

    Modificación de Input en RegistrarVendedores

--- CreaJ-2024/resources/views/RegistrarVendedorAdmin.blade.php
+++ CreaJ-2024/resources/views/RegistrarVendedorAdmin.blade.php
@@ -10,25 +10,42 @@
     <section>
         <div class="w-72 h-96 mx-auto mt-16">
             <div class="text-center">
-                <h1 class="text-3xl font-bold text-red-700">Registrar vendedor</h1>
+                <h1 class="text-3xl font-bold text-red-500">Registrar vendedor</h1>
+                
             </div>
+            
             <div class="mt-20 space-y-4">
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ubicación">
-                </div>
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción">
-                </div>
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Mercado">
-                </div>
                 <div class="flex justify-between">
                     <label for="file-input" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
-                        <span>Imagen del mercado</span>
+                        <span class="text-gray-400">Foto del Vendedor</span>
                         <input id="file-input" type="file" class="hidden">
                         <span class="rounded-lg w-5 h-5 absolute right-2 top-2 bg-cover" style="background-image: url('{{ asset('imgs/files2.svg') }}');"></span>
                     </label>
                 </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese los Nombres">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese el Apellido">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese el Correo Electrónico">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese el Número de Teléfono">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Escriba el Número de Puesto">
+                </div>
+                <div class="flex justify-center">
+                    <select class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 text-gray-400" 
+                    >
+                    <option value="NULL">Escoga el Mercado Asignado</option>
+                    <option value="SagCor">Sagrado Corazón</option>
+                    <option value="ExCua">Ex-Cuartel</option>
+                    <option value="Cen">Central</option></select>
+                </div>
+                
             </div>
         
             <div class="flex justify-center mt-16">
