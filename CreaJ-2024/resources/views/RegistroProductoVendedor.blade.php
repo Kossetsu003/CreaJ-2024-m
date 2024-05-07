1d046276d3a7c9b6eb5d1ab9577de827e4d0c5c3
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-05-06 23:01:39 -0600

    Update RegistroProductoVendedor.blade.php

--- CreaJ-2024/resources/views/RegistroProductoVendedor.blade.php
+++ CreaJ-2024/resources/views/RegistroProductoVendedor.blade.php
@@ -12,25 +12,36 @@
         <div class="w-72 h-96 mx-auto mt-16">
             <div class="text-center">
                 <h1 class="text-3xl font-bold text-black">Registrar Producto</h1>
-                <h3 class="mt-5">Nombre de producto seleccionado</h3>
+                <h3 class="mt-5">Puesto de : <b>Maria Jose Gamez</b></h3>
             </div>
             <div class="mt-20 space-y-4">
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ubicación">
-                </div>
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción">
-                </div>
-                <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Mercado">
-                </div>
                 <div class="flex justify-between">
                     <label for="file-input" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
-                        <span>Imagen del mercado</span>
+                        <span class="text-gray-600">Imagen del Producto @production
+
+                        @endproduction</span>
                         <input id="file-input" type="file" class="hidden">
                         <span class="rounded-lg w-5 h-5 absolute right-2 top-2 bg-cover" style="background-image: url('{{ asset('imgs/files2.svg') }}');"></span>
                     </label>
                 </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Producto">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción del Producto">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="number" placeholder="Precio del Producto">
+                </div>
+                <div class="flex justify-center">
+                    <select class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 text-gray-400" >
+
+                        <option value="NULL">Seleccione el Estado</option>
+                        <option value="Disponible">Disponible</option>
+                        <option value="Agotado">Agotado</option>
+                    </select>
+                </div>
+
             </div>
 
             <div class="flex justify-center mt-16">
