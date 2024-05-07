80a3a0b6cc98232269bec1fdd470e6c8da8c2394
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-05-06 21:40:12 -0600

    Modificar HomeUser

--- CreaJ-2024/resources/views/HomeMercadoUser.blade.php
+++ CreaJ-2024/resources/views/HomeMercadoUser.blade.php
@@ -10,6 +10,13 @@
 <body>
     <div class="mx-auto max-w-lg mt-10 mb-32"> <!-- Añadido un margen inferior -->
         <div class="bottom-bar fixed bottom-[5%] left-0 right-0 flex justify-center">
+
+
+
+
+            <!--INICIO DE NAVBAR MOBIL-->
+
+
             <div class="bg-gray-800 rounded-2xl w-60 h-10 flex justify-around">
                 <div class="flex items-center  ">
                     <button class=" bg-white rounded-full p-[0.25rem] "><img class="w-6" src="{{ asset('imgs/HomeSelectedIcon.png') }}" alt="User Icon"></button>
@@ -26,6 +33,11 @@
                     <button><img class="w-4" src="{{ asset('imgs/UserIcon.png') }}" alt="User Icon"></button>
                 </div>
             </div>
+
+            <!--FIN DE NAVBAR MOBIL-->
+
+
+
         </div>
 
         <div class="mt-14  w-[90%] mx-auto">
@@ -33,7 +45,7 @@
             <div class="flex justify-between  w-[90%] mx-auto"> <!--Contenedor Principal-->
                 <div>
                     <div>
-                        Nombre del Mercado
+                        Mercado Ex-Cuartel
                     </div>
                     <div class="font-bold">
                         Los mejores Precios
@@ -41,9 +53,11 @@
                 </div>
 
                 <div>
-                    <img class="w-5 mt-[50%]" src="{{ asset('imgs/usuario.png') }}" alt="User Icon">
+                    <img class="w-12 rounded-full " src="{{ asset('imgs/PerfilJuana.jpg') }}" alt="User Icon">
                 </div>
             </div><!--Fin Principal-->
+
+
 
         <div class="flex items-center mt-5 ">
                     <!-- Campo de búsqueda con icono de lupa -->
@@ -51,45 +65,49 @@
                     <img class="absolute left-2 top-1/2 transform -translate-y-1/2 w-5 h-5" src="{{ asset('imgs/lupa.png') }}" alt="Search Icon">
                     <input class="pl-8 px-2  w-[100%] border rounded-md border-gray-500 py-1 focus:outline-none" type="text" placeholder="Buscar">
                 </div>
+
+
                 <!-- Botón azul -->
                 <div class="mr-5 pl-2">
                     <button class="bg-blue-600 rounded-md px-2 py-1 h-[100%] ml-1"> <!-- Reduje el margen izquierdo a 1 -->
-                        <img class="w-6 "   src="{{ asset('imgs/casa2.png') }}" alt="User Icon">
+                        <img class="w-6 "   src="{{ asset('imgs/SettingIcon.png') }}" alt="User Icon">
                     </button>
                  </div>
         </div>
         <div class="flex mt-5">
             <div class="flex mx-auto">
                 <button class="flex items-center h-8 border text-black px-1 py-0.5 rounded-md mr-2 text-xs hover:bg-blue-200">
-                    <img class="w-3" src="{{ asset('imgs/usuario.png') }}" alt="User Icon">
+                    <img class="w-5" src="{{ asset('imgs/Box.png') }}" alt="User Icon">
                     <span class="ml-1">Todos Los puestos</span>
                 </button>
-                <button class="flex items-center border text-black px-1 py-0.5 rounded-md mr-2 text-xs hover:bg-blue-200">
-                    <img class="w-3" src="{{ asset('imgs/usuario.png') }}" alt="User Icon">
-                    <span class="ml-1">Otro Botón</span>
+
+                <button class="flex items-center border text-black px-1 py-0.5 rounded-md mr-2 text-xs bg-blue-300 border-blue-300 text-white font-bold">
+                    <img class="w-7" src="{{ asset('imgs/ClotheIcon.png') }}" alt="User Icon">
+                    <span class="ml-1">Ropa</span>
                 </button>
+
                 <button class="flex items-center border text-black px-1 py-0.5 rounded-md text-xs hover:bg-blue-200">
-                    <img class="w-3" src="{{ asset('imgs/usuario.png') }}" alt="User Icon">
-                    <span class="ml-1">Último Botón</span>
+                    <img class="w-5" src="{{ asset('imgs/FoodSelected.png') }}" alt="User Icon">
+                    <span class="ml-1">Comida</span>
                 </button>
             </div>
         </div>
 
         <div class="flex flex-wrap justify-center mt-5 text-sm">
             <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
+                <img class="w-full h-[250px] rounded-md overflow-hidden object-cover" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
                 <h3 class="font-bold mt-5">Nombre del producto</h3>
                 <h3 class="mb-2">Nombre Adicional</h3>
                 <div class="flex justify-between">
                     <h3>Comida</h3>
                     <div class="flex items-center">
                         <h3 class="mr-2">5.0</h3>
-                        <img class="w-5" src="{{ asset('imgs/estrella.png') }}" alt="User Icon">
+                        <img class="w-5 " src="{{ asset('imgs/estrella.png') }}" alt="User Icon">
                     </div>
                 </div>
             </div>
             <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
+                <img class="w-full h-[250px] rounded-md overflow-hidden object-cover" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
                 <h3 class="font-bold mt-5">Nombre del producto</h3>
                 <h3 class="mb-2">Nombre Adicional</h3>
                 <div class="flex justify-between">
@@ -102,7 +120,7 @@
             </div>
 
             <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
+                <img class="w-full h-[250px] rounded-md overflow-hidden object-cover" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
                 <h3 class="font-bold mt-5">Nombre del producto</h3>
                 <h3 class="mb-2">Nombre Adicional</h3>
                 <div class="flex justify-between">
@@ -113,7 +131,7 @@
                     </div>
                 </div>
             </div> <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
+                <img class="w-full h-[250px] rounded-md overflow-hidden object-cover" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
                 <h3 class="font-bold mt-5">Nombre del producto</h3>
                 <h3 class="mb-2">Nombre Adicional</h3>
                 <div class="flex justify-between">
@@ -123,34 +141,13 @@
                         <img class="w-5" src="{{ asset('imgs/estrella.png') }}" alt="User Icon">
                     </div>
                 </div>
-            </div> <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
-                <h3 class="font-bold mt-5">Nombre del producto</h3>
-                <h3 class="mb-2">Nombre Adicional</h3>
-                <div class="flex justify-between">
-                    <h3>Comida</h3>
-                    <div class="flex items-center">
-                        <h3 class="mr-2">5.0</h3>
-                        <img class="w-5" src="{{ asset('imgs/estrella.png') }}" alt="User Icon">
-                    </div>
-                </div>
-            </div> <div class="w-[48%] mb-8 p-2">
-                <img class="w-full h-[250px] rounded-md" src="{{ asset('imgs/ElSalvador.jpg') }}" alt="User Icon">
-                <h3 class="font-bold mt-5">Nombre del producto</h3>
-                <h3 class="mb-2">Nombre Adicional</h3>
-                <div class="flex justify-between">
-                    <h3>Comida</h3>
-                    <div class="flex items-center">
-                        <h3 class="mr-2">5.0</h3>
-                        <img class="w-5" src="{{ asset('imgs/estrella.png') }}" alt="User Icon">
-                    </div>
-                </div>
-            </div>
+
+
         </div>
-        
-    
-        
-        
+
+
+
+
     </div>
 
 </body>
