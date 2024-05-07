04aa3fb09a0c88e2b97d8b7ef710e7f6ae90f739
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-05-06 20:58:30 -0600

    Update EditarMercadoAdmin.blade.php

--- CreaJ-2024/resources/views/EditarMercadoAdmin.blade.php
+++ CreaJ-2024/resources/views/EditarMercadoAdmin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Producto Editar</title>
+    <title>Editar Mercado</title>
     <link rel="shortcut icon" href="{{ asset('imgs/MiCarritoUser.png') }}" type="image/x-icon">
 </head>
 <body>
@@ -12,20 +12,32 @@
         <div class="w-72 h-96 mx-auto mt-16">
             <div class="text-center">
                 <h1 class="text-3xl font-bold text-red-700">Editor de Mercado</h1>
-                <h3 class="text-sm mt-2">Nombre de Mercado</h3>
+                <h3 class="text-sm mt-2">Mercado Ex-Cuartel <span class="font-bold">ID: #EXC07098</span></h3>
             </div>
             <div class="mt-20 space-y-4">
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ubicación">
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Mercado">
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción">
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción del Mercado">
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Mercado">
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Distrito Ubicado">
+                </div>
+                <div class="flex justify-center">
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ubicación Específica">
+                </div>
+                <div class="flex justify-center">
+                    <h4 class="text-gray-600 text-xs px-4">Hora de Entrada : </h4>
+                    <h4 class="text-gray-600 text-xs px-4">Hora de Salida : </h4>
+                </div>
+                <div class="flex justify-center">
+
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="time" name="Entrada" min="03:00" max="21:00" >
+                    <input class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="time" name="Salida" >
                 </div>
                 <div class="flex justify-between">
-                    <label for="file-input" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
+                    <label for="file-input" class="border-1 rounded-lg border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
                         <span>Imagen del mercado</span>
                         <input id="file-input" type="file" class="hidden">
                         <span class="rounded-lg w-5 h-5 absolute right-2 top-2 bg-cover" style="background-image: url('{{ asset('imgs/files2.svg') }}');"></span>
