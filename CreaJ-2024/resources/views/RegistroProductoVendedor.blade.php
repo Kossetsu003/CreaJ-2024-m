bd2b4883568f29dfee547c09e88be011293dcbac
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-05-07 09:14:54 -0600

    Update
    Update

--- CreaJ-2024/resources/views/RegistroProductoVendedor.blade.php
+++ CreaJ-2024/resources/views/RegistroProductoVendedor.blade.php
@@ -8,28 +8,18 @@
     <title>Registrar Producto Vendedor</title>
 </head>
 <body>
+    <form action="#" method="get">
     <section>
         <div class="w-72 h-96 mx-auto mt-16">
-
-
-
             <div class="bottom-bar fixed bottom-[5%] left-0 right-0 flex justify-center">
-
-
-
-
                 <!--INICIO DE NAVBAR MOBIL-->
-
-
                 <div class="bg-gray-900 rounded-2xl w-64 h-14 flex justify-around">
-                    <div class="flex items-center  ">
+                    <div class="flex items-center">
                         <a href="./HomeUser"><img class="w-6" src="{{ asset('imgs/HomeIcon.png') }}" alt="User Icon"></a>
                     </div>
-
                     <div class="flex items-center">
-                        <a href="./CarritoGeneralUser"  class=" bg-white rounded-full p-[0.25rem] "><img class="w-6" src="{{ asset('imgs/CarritoSelectedIcon.png') }}" alt="User Icon"></a>
+                        <a href="./CarritoGeneralUser" class="bg-white rounded-full p-[0.25rem]"><img class="w-6" src="{{ asset('imgs/CarritoSelectedIcon.png') }}" alt="User Icon"></a>
                     </div>
-
                     <div class="flex items-center">
                         <a href="./EstadoPedidosUser"><img class="w-6" src="{{ asset('imgs/FavIcon.png') }}" alt="User Icon"></a>
                     </div>
@@ -37,43 +27,32 @@
                         <a href="./EditarPerfilUser"><img class="w-6" src="{{ asset('imgs/UserIcon.png') }}" alt="User Icon"></a>
                     </div>
                 </div>
-
                 <!--FIN DE NAVBAR MOBIL-->
-
-
-
             </div>
-
-
-
-
 
             <div class="text-center">
                 <h1 class="text-3xl font-bold text-black">Registrar Producto</h1>
-                <h3 class="mt-5">Puesto de : <b>Maria Jose Gamez</b></h3>
+                <h3 class="mt-5">Puesto de: <b>Maria Jose Gamez</b></h3>
             </div>
             <div class="mt-20 space-y-4">
                 <div class="flex justify-between">
                     <label for="file-input" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
-                        <span class="text-gray-600">Imagen del Producto @production
-
-                        @endproduction</span>
-                        <input id="file-input" type="file" class="hidden">
+                        <span class="text-gray-600">Imagen del Producto</span>
+                        <input id="file-input" type="file" class="hidden" >
                         <span class="rounded-lg w-5 h-5 absolute right-2 top-2 bg-cover" style="background-image: url('{{ asset('imgs/files2.svg') }}');"></span>
                     </label>
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Producto">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Nombre del Producto" required>
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción del Producto">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Descripción del Producto" required>
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="number" placeholder="Precio del Producto">
+                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="number" placeholder="Precio del Producto" required>
                 </div>
                 <div class="flex justify-center">
-                    <select class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 text-gray-400" >
-
+                    <select class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 text-gray-400" required>
                         <option value="NULL">Seleccione el Estado</option>
                         <option value="Disponible">Disponible</option>
                         <option value="Agotado">Agotado</option>
@@ -85,10 +64,10 @@
             <div class="flex justify-center mt-16">
                 <button class="bg-black w-72 h-10 text-white font-bold rounded-md">Guardar</button>
             </div>
-
-            
-            </div>
         </div>
     </section>
+</form>
+
+
 </body>
 </html>
