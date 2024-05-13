10dca8dc4e5af44fa6b3809be8df399a7da1d230
Ruben Nieto <105886696+AntonioHrdz@users.noreply.github.com>
2024-05-13 10:00:08 -0600

    BASE DE DATOS02

---
+++ CreaJ-2024/app/Models/vistaProductos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class vistaProductos extends Model
+{
+    use HasFactory;
+}
