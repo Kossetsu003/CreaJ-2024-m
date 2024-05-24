4848002714b5e8ebd072059d8e5c64aa7b9438d0
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-05-23 23:38:21 -0600

    Update MercadoRegistrarVendedor.blade.php

--- CreaJ-2024/resources/views/MercadoRegistrarVendedor.blade.php
+++ CreaJ-2024/resources/views/MercadoRegistrarVendedor.blade.php
@@ -1,3 +1,10 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ __('Create') }} Vendedor
+@endsection
+
+@section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +23,8 @@
             </div>
 
             <div class="mt-20 space-y-4">
+                <form method="POST" action="{{ route('vendedors.store') }}"  role="form" enctype="multipart/form-data">
+                    @csrf
                 <div class="flex justify-between">
                     <label for="file-input" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 flex items-center relative">
                         <span class="text-gray-400">Foto del Vendedor</span>
@@ -24,16 +33,22 @@
                     </label>
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese los Nombres" required>
+
+                    <input required type="email" name="usuario" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 form-control @error('usuario') is-invalid @enderror" value="{{ old('usuario', $vendedor?->usuario) }}" id="usuario" placeholder="Ingrese su correo electronico">
+                    {!! $errors->first('usuario', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Ingrese el Apellido" required>
+                    <input required type="text" name="nombre" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 form-control @error('nombre') is-invalid @enderror" value="{{ old('nombre', $vendedor?->nombre) }}" id="nombre" placeholder="Nombre">
+                    {!! $errors->first('nombre', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="email" placeholder="Ingrese el Correo Electrónico" required>
+
+                    <input required type="text" name="apellidos" class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400 form-control @error('apellidos') is-invalid @enderror" value="{{ old('apellidos', $vendedor?->apellidos) }}" id="apellidos" placeholder="Apellidos">
+                    {!! $errors->first('apellidos', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
                 </div>
                 <div class="flex justify-center">
-                    <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="tel" placeholder="Ingrese el Número de Teléfono" required>
+                    <input requird type="text" name="numero_puesto" class="form-control @error('numero_puesto') is-invalid @enderror" value="{{ old('numero_puesto', $vendedor?->numero_puesto) }}" id="numero_puesto" placeholder="Numero Puesto">
+            {!! $errors->first('numero_puesto', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
                 </div>
                 <div class="flex justify-center">
                     <input class="border-1 rounded border w-80 h-9 pl-5 text-xs bg-gray-100 shadow-md border-gray-400" type="text" placeholder="Escriba el Número de Puesto" required>
@@ -46,11 +61,15 @@
                         <option value="Cen">Central</option>
                     </select>
                 </div>
+
+                <div class="flex justify-center mt-16">
+                    <button class="bg-red-600 w-72 h-10 text-white font-bold rounded-md">Registrar</button>
+                </div>
+
             </div>
 
-            <div class="flex justify-center mt-16">
-                <button class="bg-red-600 w-72 h-10 text-white font-bold rounded-md">Registrar</button>
-            </div>
+
+
 
                  <div class="bottom-bar fixed bottom-[5%] left-0 right-0 flex justify-center">
                 <div class="bg-gray-900 rounded-2xl w-64 h-14 flex justify-around">
@@ -77,3 +96,4 @@
 </form>
 </body>
 </html>
+@endsection
