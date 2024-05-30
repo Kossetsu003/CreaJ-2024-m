4f07d32a9f1b765fa98eace45ecd9a6fa1c094ec
AntonioHrdz <mario.rivera200619@gmail.com>
2024-05-29 23:51:59 -0600

    Update LoginController.php

--- CreaJ-2024/app/Http/Controllers/LoginController.php
+++ CreaJ-2024/app/Http/Controllers/LoginController.php
@@ -43,8 +43,15 @@ class LoginController extends Controller
             $request->session()->put('cliente_nombre', $cliente->nombre);
             $request->session()->put('cliente_telefono', $cliente->telefono);
 
-            // Redirigir al cliente a la página de inicio
-            return redirect()->route('UserHome')->with('success', 'Bienvenido a MiniShop');
+            // Redirigir al cliente según su rol
+            if ($cliente->ROL == 1) {
+                return redirect()->route('AdminHome')->with('success', 'Bienvenido al Panel de Administración');
+            } elseif ($cliente->ROL == 4) {
+                return redirect()->route('UserHome')->with('success', 'Bienvenido a MiniShop');
+            } else {
+                // Redirigir a una página genérica si el rol no es reconocido
+                return redirect()->route('DefaultHome')->with('success', 'Bienvenido');
+            }
         }
 
         // Autenticación fallida
