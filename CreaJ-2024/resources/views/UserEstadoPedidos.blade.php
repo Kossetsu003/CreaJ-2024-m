2612b919248cfc8f042cd56294e9e60aae2e991b
Kossetsu003 <104946590+Kossetsu003@users.noreply.github.com>
2024-05-29 18:33:15 -0600

    links, menús y footer

--- CreaJ-2024/resources/views/UserEstadoPedidos.blade.php
+++ CreaJ-2024/resources/views/UserEstadoPedidos.blade.php
@@ -11,40 +11,76 @@
 
 
       <!-- Desktop Navbar -->
-      <div class="hidden md:flex p-4 bg-white items-center justify-between shadow-md">
-        <h1 class="text-3xl md:text-4xl lg:text-5xl font-black">MiniShop</h1>
-        <div class="flex gap-8">
-            <a href="./UserHome" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Home</a>
-            <a href="./UserCarritoGeneral" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Cart</a>
-            <a href="./UserEstadoPedidos" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Favorites</a>
-            <a href="./UserProfileVista" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Profile</a>
-        </div>
-    </div>
-    <!-- Mobile Navbar -->
-    <div class="fixed bottom-0 left-0 right-0 p-4 md:hidden">
-        <div class="bg-gray-900 rounded-2xl h-14 flex justify-around">
-            <div class="flex items-center">
-                <a href="./UserHome" class="bg-white rounded-full p-1">
-                    <img class="w-6" src="{{ asset('imgs/HomeSelectedIcon.png') }}" alt="Home Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserCarritoGeneral">
-                    <img class="w-6" src="{{ asset('imgs/CarritoIcon.png') }}" alt="Cart Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserEstadoPedidos">
-                    <img class="w-6" src="{{ asset('imgs/FavIcon.png') }}" alt="Favorites Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserProfileVista">
-                    <img class="w-6" src="{{ asset('imgs/UserIcon.png') }}" alt="Profile Icon" />
-                </a>
-            </div>
-        </div>
-    </div>
+      <div
+     class="hidden md:flex p-4 bg-white items-center justify-between shadow-md"
+ >
+     <h1 class="text-3xl md:text-4xl lg:text-5xl font-black">
+         MiniShop
+     </h1>
+     <div class="flex gap-8">
+         <a
+             href="../UserHome"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Home</a
+         >
+         <a
+             href="./UserCarritoGeneral"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Cart</a
+         >
+         <a
+             href="./UserEstadoPedidos"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Favorites</a
+         >
+         <a
+             href="./UserProfileVista"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Profile</a
+         >
+     </div>
+ </div>
+ <!-- Mobile Navbar -->
+ <div class="fixed bottom-0 left-0 right-0 p-4 md:hidden">
+     <div class="bg-gray-900 rounded-2xl h-14 flex justify-around">
+         <div class="flex items-center">
+             <a href="./UserHome" class="bg-white rounded-full p-1">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/HomeSelectedIcon.png') }}"
+                     alt="Home Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserCarritoGeneral">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/CarritoIcon.png') }}"
+                     alt="Cart Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserEstadoPedidos">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/FavIcon.png') }}"
+                     alt="Favorites Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserProfileVista">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/UserIcon.png') }}"
+                     alt="Profile Icon"
+                 />
+             </a>
+         </div>
+     </div>
+ </div>
 
     <main class="p-4">
         <div class="w-full bg-white p-8 rounded-lg shadow-lg">
@@ -87,5 +123,46 @@
           </div>
           
     </main>
+    <footer class="bg-[#292526] pb-16">
+                <div class="flex flex-col gap-6 md:gap-0 md:grid grid-cols-3 text-white  p-12">
+                    <div>
+                        <h2>Contact Us</h2>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                    </div>
+                    <div>
+                        <h2>Sobre nosotros</h2>
+                        <p>Informacion que un no tenemos 
+                            pero que supongo que sera de vital 
+                            importancia para el futuro</p>
+                    </div>
+                    <div class="md:self-end md:justify-self-end pb-4">
+                        <p class="font-black text-5xl mb-4">Mini <span class="text-blue-600">Shop</span></p>
+                        <div class="flex gap-2">
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/facebook.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square  flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/google.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/linkedin.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/twitter.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" src="{{ asset('imgs/youtube.png') }}" alt="">
+                            </div>
+                      
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full h-[2px] bg-white"></div>
+            </footer>
 </body>
 </html>
--- CreaJ-2024/resources/views/UserProfileVista.blade.php
+++ CreaJ-2024/resources/views/UserProfileVista.blade.php
@@ -14,48 +14,78 @@
     <title>ProfileUser</title>
 </head>
 <body>
-     <!-- Desktop Navbar -->
-     <div class="hidden md:flex p-4 bg-white items-center justify-between shadow-md">
-      <h1 class="text-3xl md:text-4xl lg:text-5xl font-black">MiniShop</h1>
-        <div class="flex gap-8">
-            <a href="../UserHome" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Home</a>
-<<<<<<< HEAD
-            <a href="./UserCarritoGeneral" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Cart</a>
-            <a href="./UserEstadoPedidos" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Favorites</a>
-            <a href="./UserProfileVista" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Profile</a>
-=======
-            <a href="../UserCarritoGeneral" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Cart</a>
-            <a href="../UserEstadoPedidos" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Favorites</a>
-            <a href="../UserProfileVista" class="font-bold uppercase text-sm lg:text-base hover:text-gray-300">Profile</a>
->>>>>>> d5fde225d4c51f1d3534dafa2a95be073863da41
-        </div>
-    </div>
-    <!-- Mobile Navbar -->
-    <div class="fixed bottom-0 left-0 right-0 p-4 md:hidden">
-        <div class="bg-gray-900 rounded-2xl h-14 flex justify-around">
-            <div class="flex items-center">
-                <a href="./UserHome" class="bg-white rounded-full p-1">
-                    <img class="w-6" src="{{ asset('imgs/HomeSelectedIcon.png') }}" alt="Home Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserCarritoGeneral">
-                    <img class="w-6" src="{{ asset('imgs/CarritoIcon.png') }}" alt="Cart Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserEstadoPedidos">
-                    <img class="w-6" src="{{ asset('imgs/FavIcon.png') }}" alt="Favorites Icon" />
-                </a>
-            </div>
-            <div class="flex items-center">
-                <a href="./UserProfileVista">
-                    <img class="w-6" src="{{ asset('imgs/UserIcon.png') }}" alt="Profile Icon" />
-                </a>
-            </div>
-        </div>
-    </div>
-    <div>
+       <!-- Desktop Navbar -->
+       <div
+     class="hidden md:flex p-4 bg-white items-center justify-between shadow-md"
+ >
+     <h1 class="text-3xl md:text-4xl lg:text-5xl font-black">
+         MiniShop
+     </h1>
+     <div class="flex gap-8">
+         <a
+             href="../UserHome"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Home</a
+         >
+         <a
+             href="./UserCarritoGeneral"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Cart</a
+         >
+         <a
+             href="./UserEstadoPedidos"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Favorites</a
+         >
+         <a
+             href="./UserProfileVista"
+             class="font-bold uppercase text-sm lg:text-base hover:text-gray-300"
+             >Profile</a
+         >
+     </div>
+ </div>
+ <!-- Mobile Navbar -->
+ <div class="fixed bottom-0 left-0 right-0 p-4 md:hidden">
+     <div class="bg-gray-900 rounded-2xl h-14 flex justify-around">
+         <div class="flex items-center">
+             <a href="./UserHome" class="bg-white rounded-full p-1">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/HomeSelectedIcon.png') }}"
+                     alt="Home Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserCarritoGeneral">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/CarritoIcon.png') }}"
+                     alt="Cart Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserEstadoPedidos">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/FavIcon.png') }}"
+                     alt="Favorites Icon"
+                 />
+             </a>
+         </div>
+         <div class="flex items-center">
+             <a href="./UserProfileVista">
+                 <img
+                     class="w-6"
+                     src="{{ asset('imgs/UserIcon.png') }}"
+                     alt="Profile Icon"
+                 />
+             </a>
+         </div>
+     </div>
+ </div>
+     
        <div class="bg-indigo-300 h-[160px] flex items-center justify-center">
             <h3 class="font-bold text-center text-4xl">Mini<span class="text-white ml-2">Shop</span></h3>
         </div>
@@ -80,7 +110,7 @@
             <h3 class="text-xs"><b>Correo Electronico : </b>juan@juan.com</h3>
         </div>
 
-        <div class="w-[50%] mx-auto mt-16">
+        <div class="w-[50%] mx-auto m-16">
             <div class=" mx-auto flex items-center">
                 <img class="w-5" src="{{ asset('imgs/heart.png') }}" alt="User Icon">
                 <h3 class="flex-grow text-left font-bold ml-3">Historial De pedidos</h3> <!-- Alineado a la derecha -->
@@ -107,6 +137,47 @@
         </div>
 
     </div>
+    <footer class="bg-[#292526] pb-16">
+                <div class="flex flex-col gap-6 md:gap-0 md:grid grid-cols-3 text-white  p-12">
+                    <div>
+                        <h2>Contact Us</h2>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                        <p>Misiones a Futuro</p>
+                    </div>
+                    <div>
+                        <h2>Sobre nosotros</h2>
+                        <p>Informacion que un no tenemos 
+                            pero que supongo que sera de vital 
+                            importancia para el futuro</p>
+                    </div>
+                    <div class="md:self-end md:justify-self-end pb-4">
+                        <p class="font-black text-5xl mb-4">Mini <span class="text-blue-600">Shop</span></p>
+                        <div class="flex gap-2">
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/facebook.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square  flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/google.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/linkedin.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" class="invert" src="{{ asset('imgs/twitter.png') }}" alt="">
+                            </div>
+                            <div class="w-8 aspect-square flex justify-center items-center bg-white rounded-full">
+                                <img width="18" src="{{ asset('imgs/youtube.png') }}" alt="">
+                            </div>
+                      
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full h-[2px] bg-white"></div>
+            </footer>
 </body>
 </html>
 <!-- !endsection -->
