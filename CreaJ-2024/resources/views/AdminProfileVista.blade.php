a3a75348c1ebe7b7c40dcd78f99589adecf80e1b
MV242667 <102163019+Cyberking23@users.noreply.github.com>
2024-08-18 07:20:06 -0600

    Ligera Modificacion
    Ligera Modificacion

--- CreaJ-2024/resources/views/AdminProfileVista.blade.php
+++ CreaJ-2024/resources/views/AdminProfileVista.blade.php
@@ -99,14 +99,14 @@
             </div>
 
 
-            <form action="{{ route('logout') }}" method="POST">
+            <form action="{{ route('logout') }}" method="GET">
                 @csrf
                 <div class="mx-auto flex items-center mt-10">
                     <img class="w-5" src="{{ asset('imgs/tuerca.png') }}" alt="User Icon">
-                    <button type="submit" class="flex-grow text-left font-bold ml-5">Cerrar Cuenta</button> <!-- Alineado a la derecha -->
+                    <button type="submit" class="flex-grow text-left font-bold ml-5">Cerrar Cuenta</button>
+                    <!-- Alineado a la derecha -->
                 </div>
             </form>
-
 
         </div>
 
