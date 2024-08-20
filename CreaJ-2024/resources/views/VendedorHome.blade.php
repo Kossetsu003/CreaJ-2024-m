8a616aeee1662e0b0587d38232c5b3839c89b44f
Kossetsu003 <104946590+Kossetsu003@users.noreply.github.com>
2024-08-19 22:07:45 -0600

    footer que desaparece
    asd

--- CreaJ-2024/resources/views/VendedorHome.blade.php
+++ CreaJ-2024/resources/views/VendedorHome.blade.php
@@ -15,11 +15,11 @@
     <div class="hidden md:flex p-4 bg-white items-center justify-between shadow-md">
         <a href="{{ route('vendedores.index') }}">
         <h1 class="text-3xl md:text-4xl lg:text-5xl font-semibold">
-             Mini <span class="text-orange-600"><b>Vendedores</b></span>
+            Mini <span class="text-orange-600"><b>Vendedores</b></span>
         </h1>
         </a>
         <div class="flex gap-8">
-             <a href="{{ route('vendedores.index') }}"
+            <a href="{{ route('vendedores.index') }}"
                 class="font-semibold uppercase text-sm lg:text-base hover:text-gray-300 px-2 py-1">Mi Puesto</a>
             <a href="{{ route('vendedores.productos') }}"
                 class="font-semibold uppercase text-sm lg:text-base hover:text-gray-300 px-2 py-1">Mis Productos</a>
@@ -34,7 +34,7 @@
         </div>
     </div>
     <!-- Mobile Navbar -->
-   <div class="bottom-bar fixed bottom-[2%] left-0 right-0 md:hidden flex justify-center">
+    <div class="bottom-bar fixed bottom-[2%] left-0 right-0 md:hidden flex justify-center">
         <div class="bg-gray-900 rounded-2xl w-64 h-14 flex justify-around">
             <div class="flex items-center">
                 <a href="{{ route('vendedores.index') }}" class="bg-white rounded-full p-1">
@@ -58,8 +58,9 @@
             </div>
         </div>
     </div>
-    <div class="md:mt-14 mt-7 w-full mx-auto md:text-[30px]">
 
+    <div class=" ">
+    <!-- Fin del Mobile Navbar -->
 
         <h1 class="md:hidden text-3xl md:text-4xl lg:text-5xl font-black pl-2 mb-6">
             Mini <span class="text-orange-600  uppercase"><b>Vendedor</b></span>
@@ -91,7 +92,7 @@
                     {{ $vendedor->nombre_del_local }}
                 </div>
                 <div class="text-[1rem] md:font-semibold  md:text-[1.5rem] mt-2">
-                   Propietario: <b class="uppercase">{{ $vendedor->nombre}} {{ $vendedor->apellidos}}</b>
+                    Propietario: <b class="uppercase">{{ $vendedor->nombre}} {{ $vendedor->apellidos}}</b>
                 </div>
                 <div class="font-semibold text-[1rem] md:text-[1.5rem] mt-2">
                     Puesto #{{ $vendedor->numero_puesto}} - <span class="md:font-bold"> en {{ $mercadoLocal->nombre }}</span>
@@ -112,7 +113,6 @@
 
         </div>
 
-       
 
         <div class="flex mt-16 justify-around w-[90%] mx-auto">
             <a href="{{ route('vendedores.agregarproducto',  $vendedor->id) }}" class="btn btn-primary btn-sm float-right "  data-placement="left">
@@ -154,13 +154,13 @@
 
     <footer class="bg-[#292526] pb-16">
         <div class="flex flex-col gap-6 md:gap-0 md:grid grid-cols-3 text-white p-12">
-            <div>
+            <div class="hidden md:block">
                 <h2 class="font-bold">Contact Us</h2>
                 <p>Whatsapp: wa.me/50369565421</p>
                 <p>Correo Electronico: contacto@minishop.sv</p>
                 <p>Dirección: Calle Ruben Dario &, 3 Avenida Sur, San Salvador</p>
             </div>
-            <div>
+            <div class="hidden md:block">
                 <h2 class="font-bold">Sobre nosotros</h2>
                 <p>Somos un equipo de desarrollo web dedicado a apoyar a los vendedores locales y municipales en el área
                     metropolitana de San Salvador, brindando soluciones tecnológicas para fortalecer los mercados
