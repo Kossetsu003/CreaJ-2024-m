9420e5893830f0aaae3d5d635e9c93a8fb72773c
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2024-08-26 08:41:43 -0600

    Actualizar app.php

--- CreaJ-2024/config/app.php
+++ CreaJ-2024/config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://minishop-laravel.onrender.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
