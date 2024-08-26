3989fcbe4794b4750797e9666af65774a5a89c64
NietoRuben <134467171+NietoRuben@users.noreply.github.com>
2024-08-26 11:23:53 -0600

    Reapply "Merge pull request #3 from pgap22/main"
    This reverts commit 2afeb1ac8fa7d61856d0225da61e4a6501ae64b7.

--- CreaJ-2024/app/Http/Controllers/LoginController.php
+++ CreaJ-2024/app/Http/Controllers/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Vendedor;
 use App\Models\MercadoLocal;
+use App\Models\Vendedor;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
